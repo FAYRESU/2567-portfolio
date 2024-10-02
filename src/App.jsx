90bb18ce0988ece5a3af5b9bfe7648c9644f9033
FAYRESU <664259005@webmail.npru.ac.th>
2024-10-02 16:57:24 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+import ProjectsBox from "./components/ProjectsBox";
+import AboutBox from "./components/AboutBox";
+import FeaturedBox from "./components/FeaturedBox";
+import ContactBox from "./components/ContactBox";
+import Footer from "./components/Footer";
+import Typed from "typed.js";
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    /**Type Effect */
+    const typingEffect = new Typed(".typedText", {
+      strings: ["Lecturer", "Developer", "Researcher"],
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 80,
+      backDelay: 2000,
+    });
+    return () => {
+      typingEffect.destroy();
+    };
+  }, []);
+  return (
+    <>
+      <div className="App">
+        <Header />
+        <main className="wrapper">
+          <FeaturedBox />
+          <AboutBox />
+          <ProjectsBox />
+          <ContactBox />
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
